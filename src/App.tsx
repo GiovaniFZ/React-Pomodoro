809969f3026c15945136ee1abf6f9414c6a2e0e7
GiovaniFZ <giovanifinazzi@gmail.com>
2024-09-03 08:23:39 -0300

    style: Button: Apply border-radius, margin and colors on default.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Button } from "./components/Button/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
     <Button variant="primary" />
     <Button variant="secondary" />
     <Button variant="danger" />
     <Button variant="success" />
     <Button />
-    </>
+    </ThemeProvider>
   )
 }
--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -6,19 +6,12 @@ interface ButtonContainerProps {
     variant: ButtonVariant;
 }
 
-const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
-    ${props => {
-        return css`
-        background-color: ${buttonVariants[props.variant]}
-        ` 
-    }}
+    border-radius: 4px;
+    margin: 8px;
+    border: 0;
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white}
 `
