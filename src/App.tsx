550c674dec3700d29a24cf949a09c5935102ba7d
GiovaniFZ <giovanifinazzi@gmail.com>
2024-09-04 09:21:42 -0300

    feat: Implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { Button } from "./components/Button/Button";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/themes/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-    <Button variant="primary" />
-    <Button variant="secondary" />
-    <Button variant="danger" />
-    <Button variant="success" />
-    <Button />
-    <GlobalStyle />
+      <BrowserRouter>
+      <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
