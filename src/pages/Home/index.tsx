d274c3073bb182878f6ca42eb17adbe51c24acc9
GiovaniFZ <giovanifinazzi@gmail.com>
2024-09-18 12:41:22 -0300

    feat: Don't show interrupt button when cycle finish

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ import { differenceInSeconds } from "date-fns";
 export function Home() {
     const newcCycleFormSchema = zod.object({
         task: zod.string().min(1, 'Informe a tarefa'),
-        minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no mínimo 5 minutos.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
+        minutesAmount: zod.number().min(1, 'O ciclo precisa ser de no mínimo 1 minuto.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
     })
     type NewCycleFormData = zod.infer<typeof newcCycleFormSchema>
 
@@ -82,6 +82,7 @@ export function Home() {
                         })
                     )
                     setAmountSecondsPassed(totalSeconds);
+                    setActiveCycleId(null);
                     clearInterval(interval);
                 }else{
                     setAmountSecondsPassed(secondsDifference);
@@ -134,7 +135,7 @@ export function Home() {
                         id="minutesAmount"
                         placeholder="00"
                         step={5}
-                        min={5}
+                        min={1}
                         max={60}
                         disabled={!!activeCycle}
                         {...register('minutesAmount', { valueAsNumber: true })}
